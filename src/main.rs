d06ac0ef5a71281297aaff0d560fb02ef230dd94
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2023-04-29 13:53:12 +0300

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use std::io::BufRead;
 use rayon::prelude::*;
+use std::io::BufRead;
 
 fn main() {
     println!("Usage example:  echo -e 'foo \\n bar' | youtube-dl-rs");
 
     // Collect them.
-    let urls: Vec<String> = std::io::stdin().lock()
+    let urls: Vec<String> = std::io::stdin()
+        .lock()
         .lines()
-        .map(|l| {
-            match l {
-                Ok(v) => { return v.trim().to_owned(); }
-                Err(e) => { panic!("Failed to read line with error {e}"); }
+        .map(|l| match l {
+            Ok(v) => {
+                return v.trim().to_owned();
+            }
+            Err(e) => {
+                panic!("Failed to read line with error {e}");
             }
         })
         .collect();
