fac7775da6400d1ab4056aad220b3cd2150fa3c7
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2023-04-29 16:48:11 +0300

    Expecting a url argument instead of reading from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
 use rayon::prelude::*;
-use std::io::BufRead;
 
 fn main() {
-    println!("This program expects input from stdin.");
-    println!("Usage example:  echo -e 'url1 \\n url2 \\n url3' | youtube-dl-rs");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+        panic!("
+This program expects a string arguments with all the urls.
+Usage example: youtube-dl-rs 'url1 url2 url3'
+");
+    }
 
     // Collect them.
-    let urls: Vec<String> = std::io::stdin()
-        .lock()
-        .lines()
-        .map(|l| match l {
-            Ok(v) => {
-                return v.trim().to_owned();
-            }
-            Err(e) => {
-                panic!("Failed to read line with error {e}");
-            }
-        })
+    let urls: Vec<String> = args
+        .get(1)
+        .unwrap()
+        .split(' ')
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|v| v.trim().to_owned())
+        .filter(|v| !v.is_empty())
         .collect();
 
     // Download them.
